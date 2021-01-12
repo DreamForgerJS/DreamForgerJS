2e7e4fa51abf0ffb8164b050911d920daa44a670
LeviathanProgramming <connerow1115@gmail.com>
2021-01-12 11:00:44 -0600

    1.0.2

--- scripts/core.js
+++ scripts/core.js
@@ -3,12 +3,4 @@ import * as iris from './iris.js'
 import $ from './query.js'
 import body from './body.js'
 import DreamForger from './dreamforger.js'
-
-let com;
-function commandHandler(){}
-DreamForger.handleCommands = function(){
-  com = DreamForger.setCommander()
-  commandHandler()
-  iris.parseColors()
-}
-export { DreamForger, body, alg, iris, com };
+export { DreamForger, body, alg, iris };
